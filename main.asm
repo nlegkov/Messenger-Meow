143e31a75a57f02315077be4458878023c1089e9
Ruslan Bogomolov <ruslanbogomolov200755@gmail.com>
2026-03-08 03:09:01 +0700

    Uart + cdm8e + Print'Hello' port 7241

--- main.asm
+++ main.asm
@@ -1,55 +1,20 @@
 asect 0
 
-ldi r0, 17
-jsr func
-halt
+    ldi r0, 0xF0
 
+loop:
+    ldi r1, 72
+    st  r0, r1
+    ldi r1, 69
+    st  r0, r1
+    ldi r1, 76
+    st  r0, r1
+    ldi r1, 76
+    st  r0, r1
+    ldi r1, 79
+    st  r0, r1
+    ldi r1, 32
+    st  r0, r1
+    br  loop
 
-
-func:
-# сумма четных чисел до r0, не больше 50
-push r1
-push r2
-push r3
-
-move r0, r3
-xor r0, r0
-
-while
-tst r3
-stays nz
-
-    dec r3
-
-    ldi r2, 50
-
-    if 
-    cmp r0, r2
-    is gt
-        break
-    fi
-
-    ldi r2, 1
-    if
-    and r3, r2
-    tst r2
-    is nz
-        continue
-    fi
-
-    add r3, r0
-    
-
-wend
-
-
-pop r3
-pop r2
-pop r1
-rts
-
-
-
-
-halt
 end
