7ef6e18a073e34878c0e61058214d722bb05e701
Ruslan Bogomolov <ruslanbogomolov200755@gmail.com>
2026-03-20 10:54:35 +0700

    lolo

--- main.asm
+++ main.asm
@@ -6,37 +6,14 @@ uart_handler:    ext
 
 dc main,            0
 dc default_handler, 0
-dc default_handler, 0
+dc default_hxxxandler, 0
 dc default_handler, 0
 dc default_handler, 0
 dc uart_handler,    0
 
 align 0x80
 
-rsect exc_handlers
-
-default_handler>
-    halt
-
-uart_handler>
-    push r0
-    push r1
-
-    ldi  r0, 0x7F80
-    ldb  r0, r1
-    stb  r0, r1
-
-    pop  r1
-    pop  r0
-    rti
-
-rsect main
-
-main>
-    ei
-
-main_loop>
-    wait
-    br   main_loop
+ldi r0, main
+jmp r0
 
 end.
