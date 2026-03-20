a09face3b44d69a236c89623f5627d8bfd3c0c36
Ruslan Bogomolov <ruslanbogomolov200755@gmail.com>
2026-03-20 10:05:23 +0700

    test2

--- main.asm
+++ main.asm
@@ -1,20 +1,42 @@
 asect 0
 
-    ldi r0, 0xF0
+main:            ext
+default_handler: ext
+uart_handler:    ext
 
-loop:
-    ldi r1, 72
-    st  r0, r1
-    ldi r1, 69
-    st  r0, r1
-    ldi r1, 76
-    st  r0, r1
-    ldi r1, 76
-    st  r0, r1
-    ldi r1, 79
-    st  r0, r1
-    ldi r1, 32
-    st  r0, r1
-    br  loop
+dc main,            0
+dc default_handler, 0
+dc default_handler, 0
+dc default_handler, 0
+dc default_handler, 0
+dc uart_handler,    0
 
-end
+align 0x80
+
+rsect exc_handlers
+
+default_handler>
+    halt
+
+uart_handler>
+    push r0
+    push r1
+
+    ldi  r0, 0x7F80
+    ldb  r0, r1
+    stb  r0, r1
+
+    pop  r1
+    pop  r0
+    rti
+
+rsect main
+
+main>
+    ei
+
+main_loop>
+    wait
+    br   main_loop
+
+end.
